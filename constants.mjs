e5d23f8036c325f24a48389ba2b7920df6926c6a
cklgaming <colinklassey33@gmail.com>
2024-06-21 16:35:01 -0400

    Basic decider

--- constants.mjs
+++ constants.mjs
@@ -1,1 +1,22 @@
 // constants.mjs
+
+export const players = [
+    {
+        id: 0,
+        name: "Jeff Teat",
+        position: "A",
+        rating: 8
+    },
+    {
+        id: 1,
+        name: "Gavin Adler",
+        position: "D",
+        rating: 6
+    },
+    {
+        id: 2,
+        name: "Tim Troutner",
+        position: "G",
+        rating: 8
+    },
+]
--- index.mjs
+++ index.mjs
@@ -1,5 +1,24 @@
 // index.mjs
+//import { players } from "./constants.mjs";
 
 const p1 = document.querySelector("#p1");
+const button1 = document.querySelector("#button1");
 
-p1.innerText = "Hello, world!";
+const attackName = prompt(`Input the name of the attacker`);
+const attackRating = prompt(`Input the rating of the attacker`);
+const defenseName = prompt(`Input the name of the defender`);
+const defenseRating = prompt(`Input the rating of the defender`);
+const goalieName = prompt(`Input the name of the goalie`);
+const goalieRating = prompt(`Input the rating of the goalie`);
+
+p1.innerText = `${attackName}, ${attackRating}, ${defenseName}, ${defenseRating}, ${goalieName}, ${goalieRating}`;
+button1.onclick = startGame;
+
+function startGame() {
+    if (attackRating > defenseRating) {
+        p1.innerText = `Goal! ${attackName}'s shot hit the back of the net!`;
+    }
+    else if (attackRating < defenseRating) {
+        p1.innerText = `Shut down! ${defenseName} locked up the attacker and the shot clock expired!`;
+    }
+}
