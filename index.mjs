915e62fe617d46eb0c03a10e684372975ff89a34
cklgaming <colinklassey33@gmail.com>
2024-06-22 21:17:36 -0400

    Expanded decider, made player selection more dynamic

--- index.mjs
+++ index.mjs
@@ -1,34 +1,74 @@
 // index.mjs
-//import { players } from "./constants.mjs";
+import { players } from "./constants.mjs";
 
 const p1 = document.querySelector("#p1");
 const button1 = document.querySelector("#button1");
 
-const attackName = prompt(`Input the name of the attacker`);
-const attackRating = prompt(`Input the rating of the attacker`);
-const defenseName = prompt(`Input the name of the defender`);
-const defenseRating = prompt(`Input the rating of the defender`);
-const goalieName = prompt(`Input the name of the goalie`);
-const goalieRating = prompt(`Input the rating of the goalie`);
+let attack = {
+    id: null,
+    name: null,
+    rating: null
+}
 
-const ratings = [attackRating, defenseRating, goalieRating];
+let defense = {
+    id: null,
+    name: null,
+    rating: null
+}
 
-p1.innerText = `${attackName}, ${attackRating}, ${defenseName}, ${defenseRating}, ${goalieName}, ${goalieRating}`;
+let goalie = {
+    id: null,
+    name: null,
+    rating: null
+}
+
+attack.id = prompt(`Input the id of the attacker`);
+matchPlayer(attack);
+defense.id = prompt(`Input the id of the defender`);
+matchPlayer(defense);
+goalie.id = prompt(`Input the id of the goalie`);
+matchPlayer(goalie);
+
+let ids = [attack.id, defense.id, goalie.id];
+let ratings = [];
+
 button1.innerText = "Start Game";
 button1.onclick = startGame;
 
 function startGame() {
+    console.log(`Starting game...`);
+    console.log(ratings);
     randomize();
-    if (attackRating > defenseRating) {
+    if (attackRating > defenseRating && attackRating > goalieRating) {
         p1.innerText = `Goal! ${attackName}'s shot hit the back of the net!`;
     }
-    else if (attackRating < defenseRating) {
-        p1.innerText = `Shut down! ${defenseName} locked up the attacker and the shot clock expired!`;
+    else if (attackRating > defenseRating && attackRating < goalieRating) {
+        p1.innerText = `Saved! ${attackName} was able to get a shot on goal, but it was saved by ${goalieName}!`;
     }
+    else if (attackRating < defenseRating) {
+        p1.innerText = `Shut Down! {$defenseName} locked up ${attackName} and the shot clock expired!`;
+    }
+    updateGame();
 }
 
 function randomize() {
     for (let i = 0; i < ratings.length; i++) {
         ratings[i] = Math.floor(ratings[i] * Math.random() * (1.1 - 0.7) + 0.7);
+    }
+}
+
+function updateGame() {
+    matchPlayer(attack);
+    matchPlayer(defense);
+    matchPlayer(goalie);
+}
+
+function matchPlayer(player) {
+    for (let i = 0; i < players.length; i++) {
+        if (player.id == players[i].id) {
+            player.name = players[i].name;
+            player.rating = players[i].rating;
+            ratings.append(players[i].rating);
+        }
     }
 }
